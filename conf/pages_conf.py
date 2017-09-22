e71bc14b6d5287cb8541d03eea7d767bc5c3fb53
yt7589 <yt7589@qq.com>
2017-09-22 16:30:43 +0800

    t

--- conf/pages_conf.py
+++ conf/pages_conf.py
@@ -13,6 +13,41 @@ pages_conf = {
             "right": "",
             "footer": "footer.html"
         },
+        "show_img_csf": {
+            "header": "header.html",
+            "left": "",
+            "body": "cv/img_csf.html",
+            "right": "",
+            "footer": "footer.html"
+        },
+        "show_img_idf": {
+            "header": "header.html",
+            "left": "",
+            "body": "cv/img_idf.html",
+            "right": "",
+            "footer": "footer.html"
+        },
+        "show_img_seg": {
+            "header": "header.html",
+            "left": "",
+            "body": "cv/img_seg.html",
+            "right": "",
+            "footer": "footer.html"
+        },
+        "show_time_sq": {
+            "header": "header.html",
+            "left": "",
+            "body": "fai/stock_hp.html",
+            "right": "",
+            "footer": "footer.html"
+        },
+        "show_mine": {
+            "header": "header.html",
+            "left": "",
+            "body": "mine/mine.html",
+            "right": "",
+            "footer": "footer.html"
+        },
         "show_theory": {
             "header": "header.html",
             "left": "",

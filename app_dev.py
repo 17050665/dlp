fe9770cef7de1f45be9070e59e4dbd2ff7e74bb6
yt7589 <yt7589@qq.com>
2017-09-22 15:01:39 +0800

    adjust

--- app_dev.py
+++ app_dev.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('./lib/cherrypy')
 sys.path.append('./lib/jinja')
+sys.path.append('./lib/tf/slim')
 import os
 import cherrypy
 import app_global as ag
@@ -11,6 +12,7 @@ import common.wky_queues as wqs
 #from apps.esp.esp_main import EspMain as EspMain
 #from apps.rgl.rgl_main import RglMain as RglMain
 from apps.gile.udp_server import UdpServer as UdpServer
+from ann.cnn.slim_inresv2 import SlimInresV2 as SlimInresV2
 
 def test1():
     # 试验读取静态方法
@@ -20,11 +22,15 @@ def test1():
     
 def test_esp():
     #EspMain.startup({})
-    RglMain.startup({})
+    #RglMain.startup({})
+    pass
     
 if __name__ == '__main__':
     print('starting up...')
     #test1()
     #test_esp()
-    UdpServer.startup('', 8088, 1024)
+    # UdpServer.startup('', 8088, 1024)o
+    inres = SlimInresV2()
+    #inres.predict('d:/awork/d3.jpg')
+    inres.predict('./work/t001.jpg')
     
